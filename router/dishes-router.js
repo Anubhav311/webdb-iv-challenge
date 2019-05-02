560ee762dac44ce24480c70e8a120c91b7035476
Anubhav311 <anubhavbhambri1@gmail.com>
2019-05-03 04:53:41 +0530

    Completed seeding for dishes

--- router/dishes-router.js
+++ router/dishes-router.js
@@ -8,8 +8,18 @@ router.get('/', (req, res) => {
       res.status(200).json(dishes);
     })
     .catch(error => {
-      res.status(500).json({ message: 'We ran into an error retrieving the tracks' });
+      res.status(500).json({ message: 'We ran into an error retrieving the dishes' });
     });
 });
+
+router.post('/', (req, res) => {
+    Dishes.add(req.body)
+      .then(dish => {
+        res.status(200).json(dish);
+      })
+      .catch(error => {
+        res.status(500).json({ message: 'We ran into an error adding the dish' });
+      });
+  });
 
 module.exports = router;
